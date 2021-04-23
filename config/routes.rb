0a128aaae0e858e96d70c56cb37662e60588a9c2
KQué <77649679+KQue6@users.noreply.github.com>
2021-04-22 23:13:38 -0700

    Articles Listing Added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'home', to: 'pages#home'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 end
