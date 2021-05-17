6248eb261e6a01c5a15e7850af7abbcf7ba22557
KQué <77649679+KQue6@users.noreply.github.com>
2021-05-16 20:38:49 -0700

    KQue Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
   class User < ApplicationRecord
-
+    validates :username, presence: true, 
+                        uniqueness: { case_senitive: false }, 
+                        length: {minimum: 4, maximum:29 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, 
+                        uniqueness: { case_senitive: false }, 
+                        length: { maximum: 109 },
+                        format: { with: VALID_EMAIL_REGEX }
   end
