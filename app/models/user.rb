ca9ebdb0532d23ff71add569701f3aaef822ff99
KQué <77649679+KQue6@users.noreply.github.com>
2021-05-20 11:01:20 -0700

    Salt Was Added To User Passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
   class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     validates :username, presence: true, 
                         uniqueness: { case_senitive: false }, 
@@ -8,4 +9,5 @@
                         uniqueness: { case_senitive: false }, 
                         length: { maximum: 109 },
                         format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
   end
