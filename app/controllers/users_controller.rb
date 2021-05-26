f9d467445d1284dff4ab03433831376b1d1a45ac
KQué <77649679+KQue6@users.noreply.github.com>
2021-05-26 15:45:19 -0700

    Users Edit And Update Created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@
       @user = User.new
     end 
 
+    def edit 
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "You Have All The Money In The World"
+        redirect_to articles_path
+      else
+        render 'edit'
+      end
+    end
+
+
+    end
+
     def create 
       @user = User.new(user_params)
       if @user.save
@@ -19,4 +36,4 @@
       params.require(:user).permit(:username, :email, :password)
     end
 
-  end 
+  # end 
