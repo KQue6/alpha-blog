558bdc24fee9a9a4758438a773905859dd30d442
KQué <77649679+KQue6@users.noreply.github.com>
2021-06-05 19:41:08 -0700

    Q writters Edit Access only to individual writters pages/articles login/or loged out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
   class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
 
     def show 
       @articles = @user.articles.paginate(page: params[:page], per_page: 6)
@@ -47,4 +49,12 @@
     def set_user
       @user = User.find(params[:id])
     end
+
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "Q-writers Editing Rights Are For Personal Books And Pages"
+        redirect_to @user
+      end
+    end
+
   end
