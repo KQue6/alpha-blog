cac68b0873cc84f55b578581a9defbeddf18f879
KQué <77649679+KQue6@users.noreply.github.com>
2021-06-06 14:11:15 -0700

    Qwriters can now delete Personal Books

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
   class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update,:destroy]
 
     def show 
       @articles = @user.articles.paginate(page: params[:page], per_page: 6)
@@ -26,6 +26,15 @@
       else
         render 'edit'
       end
+    end
+
+
+    def destroy
+      @user.destroy
+      session[:user_id] = nil
+      flash[:notice] = "Q-Writer's Book Has Been Deleted"
+      redirect_to articles_path
+
     end
 
 
