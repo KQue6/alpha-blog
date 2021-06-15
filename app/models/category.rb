fa3b583a83e051fa4e1e12fda8876292e7ae799b
KQué <77649679+KQue6@users.noreply.github.com>
2021-06-15 13:36:29 -0700

    Created New Migration File For Categories And Many to Many Links

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true,length: { minimum: 4, maximum: 36 }
   validates_uniqueness_of :name
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
